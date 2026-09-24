f1d632988629849d401841c673430b5348d2d12c
Yan Kauê <147212233+YanKaue10@users.noreply.github.com>
2026-09-23 22:25:29 -0300

    feat: Adicionando a verificação de Convergência

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,48 @@ public class Main {
                 {1, 5, 1},
                 {2, 3, 10}
         };
-        int[] B = {7, -8, 10};
+        int[] B = {7, -8, 6};
         double[] X = {0, 0, 0}; // Chute inicial
+        boolean converge = true;
 
-//        double precisao = 0.05;
+        //Bloco 1 : Verificar a Convergencia
 
-        for (int iter = 0; iter < 3; iter++) {
+        for (int i = 0; i < A.length; i++) {
+            int somaConvergencia = 0;
+            for (int j = 0; j < A[i].length; j++) {
+                if (j != i) {
+                    somaConvergencia += Math.abs(A[i][j]);
+                }
+            }
+            if (Math.abs(A[i][i]) > somaConvergencia) {
+                converge = true;
+                System.out.println("O Sistema Converge");
+            } else {
+                System.out.println("Não Converge");
+            }
+        }
+
+        //Bloco 2: Gauss Seidel
+
+        for (int iter = 0; iter < 10; iter++) {
 
             for (int i = 0; i < X.length; i++) {
+
                 double soma = 0.0;
 
                 for (int j = 0; j < A[i].length; j++) {
                     if (j != i) {
-                       soma += A[i][j] * X[j];
+                        soma += A[i][j] * X[j];
                     }
-                }
                     X[i] = (B[i] - soma) / A[i][i];
+                }
             }
             System.out.println("Iteração " + (iter + 1));
-            System.out.println("X = " + X[0]);
-            System.out.println("Y = " + X[1]);
-            System.out.println("Z = " + X[2]);
+            System.out.println("X1 = " + X[0]);
+            System.out.println("X2= " + X[1]);
+            System.out.println("X3 = " + X[2]);
             System.out.println("-------------------");
         }
     }
 }
+
